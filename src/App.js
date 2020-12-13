ba242442fe6074c0a1fbd6161b95dc7a6ada4ed0
Jordan Belmont <jordan.belmont@hotmail.com>
2020-12-13 21:21:48 +0000

    Created new component in Todos.js and passed them over to App.js via props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Todos from './Todos';
 
 class App extends Component {
    state = {
@@ -13,6 +13,7 @@ class App extends Component {
       return (
          <div className="todo-app container">
             <h1 className="center blue-text">Todo's</h1>
+            <Todos todos={this.state.todos}/>
          </div>
       );
    }
