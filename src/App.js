c61102b7f74c4edcbb4609c7c1ae98613bee6bef
Jordan Belmont <jordan.belmont@hotmail.com>
2020-12-18 21:10:05 +0000

    created form function with hooks in Form.js, enabled useState in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
+import TodoList from './components/TodoList'
 import './App.css';
 
 function App() {
+   const [inputText, setInputText] = useState("");
+   const [todos, setTodos] = useState([]);
+
    return (
       <div className="App">
          <header>
             <h1>Wanna Do List</h1>
          </header>
-         <Form />
+         <Form setInputText={setInputText} />
+         <TodoList />
       </div>
    );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ setInputText }) => {
+   const inputTextHandler = (e) => {
+      console.log(e.target.value);
+      setInputText(e.target.value);
+   };
+
    return(
       <form>
-         <input type="text" className="todo-input" />
+         <input 
+         onChange= {inputTextHandler} 
+         type="text" 
+         className="todo-input" 
+         />
          <button type="submit" className="todo-button">
             <i className="fas fa-plus-square"></i>
          </button>
          <div className="select">
             <select name="todos" className="filter-todo">
                <option value="all">All</option>
-               <option value="Complete">All</option>
-               <option value="Uncomplete">All</option>
+               <option value="Complete">Complete</option>
+               <option value="Uncomplete">Uncomplete</option>
             </select>
          </div>
       </form>
